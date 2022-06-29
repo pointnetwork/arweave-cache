f671c789adfa0e1ba3d6359ab5f9b4d10a5c9f1a
Brian Alonso <brian.alonso.a@gmail.com>
2022-06-28 21:23:25 -0300

    Fix lint workflow

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,7 @@
-import { getFileFromS3, health, isUploaded } from "./handlers";
+import { getFileFromS3, health, isUploaded } from './handlers';
 
 export default (_, router) => {
-    router.get('/health', health);
-    router.get('/download/:chunkId', getFileFromS3);
-    router.get('/is_uploaded/:chunkId', isUploaded);
+  router.get('/health', health);
+  router.get('/download/:chunkId', getFileFromS3);
+  router.get('/is_uploaded/:chunkId', isUploaded);
 };
